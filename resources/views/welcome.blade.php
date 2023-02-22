b42e356180765c090591abfbd222f1c360e4d2db
MuizzuddinRifqi <muizzuddinrifqi@gmail.com>
2023-02-22 14:57:22 +0700

    First Trial ho

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+
+    <!-- Styles -->
+    <style>
+        body {
+            font-family: 'Nunito';
+        }
+
+        .container {
+            background-color: #f8f9fa;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .centered-text {
+            text-align: center;
+        }
+
+        .centered-text h1 {
+            font-size: 4rem;
+            margin: 0;
+            padding: 0;
+            color: #fff;
+            background-color: #007bff;
+            display: inline-block;
+            padding: 10px 20px;
+            border-radius: 5px;
+        }
+
+        .centered-text p {
+            font-size: 2rem;
+            margin: 0;
+            padding: 0;
+            color: #333;
+        }
+    </style>
+</head>
+
+<body class="antialiased">
+    <div class="container">
+        <div class="centered-text">
+            <h1>SELAMAT DATANG</h1>
+            <p>M. MUIZZUDDIN RIFQI</p>
+            <P>(Wong Ganteng)</P>
+        </div>
+    </div>
+</body>
+
+</html>
